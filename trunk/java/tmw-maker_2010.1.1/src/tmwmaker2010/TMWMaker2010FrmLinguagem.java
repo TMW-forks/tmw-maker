add2f05689bc72d897c9c95b1eb6c842d93ad318
rui.gravata <rui.gravata@1b849053-5f8f-d5e0-c778-10af80e1e48d>
2010-03-21 02:46:32 +0000

    Correção do número da Versão de "2010.0.1" para "2010.1.1"

--- trunk/java/tmw-maker_2010.1.1/src/tmwmaker2010/TMWMaker2010FrmLinguagem.java
+++ trunk/java/tmw-maker_2010.1.1/src/tmwmaker2010/TMWMaker2010FrmLinguagem.java
@@ -32,29 +32,47 @@ public class TMWMaker2010FrmLinguagem extends javax.swing.JFrame {
    private void initComponents() {
 
       jToggleButton1 = new javax.swing.JToggleButton();
+      jScrollPane1 = new javax.swing.JScrollPane();
+      jList1 = new javax.swing.JList();
 
       setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
       org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(tmwmaker2010.TMWMaker2010App.class).getContext().getResourceMap(TMWMaker2010FrmLinguagem.class);
-      setTitle(resourceMap.getString("Form.title")); // NOI18N
-      setName("Form"); // NOI18N
+      setTitle(resourceMap.getString("FrmLinguagens.title")); // NOI18N
+      setName("FrmLinguagens"); // NOI18N
+      setResizable(false);
 
       jToggleButton1.setMnemonic('R');
       jToggleButton1.setText(resourceMap.getString("BtnFechar.text")); // NOI18N
       jToggleButton1.setName("BtnFechar"); // NOI18N
+
+      jScrollPane1.setName("jScrollPane1"); // NOI18N
+
+      jList1.setModel(new javax.swing.AbstractListModel() {
+         String[] strings = { "Português (Brasil)", "English" };
+         public int getSize() { return strings.length; }
+         public Object getElementAt(int i) { return strings[i]; }
+      });
+      jList1.setName("LstLinguagens"); // NOI18N
+      jList1.setSelectedIndex(0);
+      jScrollPane1.setViewportView(jList1);
 
       javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
       getContentPane().setLayout(layout);
       layout.setHorizontalGroup(
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
          .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-            .addContainerGap(176, Short.MAX_VALUE)
-            .addComponent(jToggleButton1)
+            .addContainerGap()
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+               .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE)
+               .addComponent(jToggleButton1))
             .addContainerGap())
       );
       layout.setVerticalGroup(
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
          .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-            .addContainerGap(123, Short.MAX_VALUE)
+            .addContainerGap()
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(jToggleButton1)
             .addContainerGap())
       );
@@ -74,6 +92,8 @@ public class TMWMaker2010FrmLinguagem extends javax.swing.JFrame {
     }
 
    // Variables declaration - do not modify//GEN-BEGIN:variables
+   private javax.swing.JList jList1;
+   private javax.swing.JScrollPane jScrollPane1;
    private javax.swing.JToggleButton jToggleButton1;
    // End of variables declaration//GEN-END:variables
 
