a6194ad3a4cd75f6323e7d50bc64aaf3a3d11d73
rui.gravata <rui.gravata@1b849053-5f8f-d5e0-c778-10af80e1e48d>
2010-04-27 17:02:56 +0000

    Renomear "FrmScript" para "FrmPalco". O Nome "FrmScript" será usado para outro tipo de formulário.

--- trunk/java/TMW-Maker 0.2/src/Formularios/FrmPrincipal.java
+++ trunk/java/TMW-Maker 0.2/src/Formularios/FrmPrincipal.java
@@ -907,13 +907,13 @@ public class FrmPrincipal extends javax.swing.JFrame {
         ExecutarJogo();
     }//GEN-LAST:event_MnuJogoExecutarActionPerformed
     private void MnuEditarPersonagemScriptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MnuEditarPersonagemScriptActionPerformed
-        javax.swing.JDialog FrmNpcScript = new FrmPalco(this, rootPaneCheckingEnabled);
-        FrmNpcScript.setLocation(
-                ((this.getWidth() - FrmNpcScript.getWidth()) / 2) + this.getX(),
-                ((this.getHeight() - FrmNpcScript.getHeight()) / 2) + this.getY());
-        FrmNpcScript.pack();
-        FrmNpcScript.setModal(true);
-        FrmNpcScript.setVisible(true);/**/
+        javax.swing.JDialog FrmScript = new FrmScript(this, rootPaneCheckingEnabled);
+        FrmScript.setLocation(
+                ((this.getWidth() - FrmScript.getWidth()) / 2) + this.getX(),
+                ((this.getHeight() - FrmScript.getHeight()) / 2) + this.getY());
+        FrmScript.pack();
+        FrmScript.setModal(true);
+        FrmScript.setVisible(true);/**/
     }//GEN-LAST:event_MnuEditarPersonagemScriptActionPerformed
     private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
         if (Config.getOS().indexOf("win") >= 0) {
