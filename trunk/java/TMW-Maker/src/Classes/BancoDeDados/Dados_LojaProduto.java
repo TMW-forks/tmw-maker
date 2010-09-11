fbfc350a5de386b9fad0e4bf80473f3875bad3a0
rui.gravata <rui.gravata@1b849053-5f8f-d5e0-c778-10af80e1e48d>
2010-09-11 02:46:26 +0000

    FEITO:  * Tentativa de criar editor de Lojas

--- trunk/java/TMW-Maker/src/Classes/BancoDeDados/Dados_LojaProduto.java
+++ trunk/java/TMW-Maker/src/Classes/BancoDeDados/Dados_LojaProduto.java
@@ -1,11 +1,16 @@
 package Classes.BancoDeDados;
 
-public class DadosLojaProduto {
+public class Dados_LojaProduto {
     private int produtoID=0;
     private int produtoPrecoDeVenda=0;
+
+    Dados_LojaProduto(int NovoID, int NovoPrecoDeVenda) {
+        produtoID=NovoID;
+        produtoPrecoDeVenda=NovoPrecoDeVenda;
+    }
     
     public void setID(int NovoID){produtoID=NovoID;}
-    public void setPrecoDeVenda(int NovoPreco){produtoPrecoDeVenda=NovoPreco;}
+    public void setPrecoDeVenda(int NovoPrecoDeVenda){produtoPrecoDeVenda=NovoPrecoDeVenda;}
 
     public int getID(){return produtoID;}
     public int getPrecoDeVenda(){return produtoPrecoDeVenda;}
