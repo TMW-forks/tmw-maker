5c57395ee2c996150c74c78011de364e800a92ea
rui.gravata <rui.gravata@1b849053-5f8f-d5e0-c778-10af80e1e48d>
2010-09-11 02:04:50 +0000

    FEITO:  * Tentativa de criar editor de Lojas

---
+++ java/TMW-Maker/src/Classes/BancoDeDados/Dados_Lojas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Classes.BancoDeDados;
+
+/**
+ *
+ * @author indigovox
+ */
+public class Dados_Lojas {
+    private String nomeNPC="";
+    private int imagemNPC=0;
+    private String mapaNome="";
+    private int mapaCoordX=0;
+    private int mapaCoord0=0;
+
+}
