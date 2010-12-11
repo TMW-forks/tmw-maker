6ac413627a8f75bdcd07a48db037825885107744
rui.gravata <rui.gravata@1b849053-5f8f-d5e0-c778-10af80e1e48d>
2010-12-11 18:29:20 +0000

    FEITO:  * Adicionado "src/autocomplete/comandos.xml" ao projeto.
    DEFEITO:
     * Não estou conseguindo compilar o projeto! (X_x)

--- java/TMW-Maker/src/autocomplete/DocComando.java
+++ java/TMW-Maker/src/autocomplete/DocComando.java
@@ -4,6 +4,8 @@
  */
 
 package autocomplete;
+import Classes.DialogClass;
+import Classes.FileClass;
 import java.net.URISyntaxException;
 import java.util.List;  
   
@@ -22,37 +24,38 @@ public class DocComando {
     XStream xstream = new XStream();  
       
     public List resgatarXML() throws URISyntaxException{
-       
         /*O ARQUIVO COMANDOS.XML DEVE QUE ESTAR DO LADO DO .JAR, OU SEJA NA MESMA PASTA*/
         //File file = new File(DocComando.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().replaceAll("JQuestEdit.jar", "")+"comandos.xml");
-      File file = null;
-
-
-      
+        File file = null;
+        String Endereco="";
+        Endereco=DocComando.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().replaceAll("TMW-Maker.jar", "") + "comandos.xml";
+        if(!FileClass.seExiste(Endereco)) Endereco="src/autocomplete/comandos.xml"; //vai buscar o arquivo comando na raiz do projeto. (Como uma segunda auternativa ao endereço não encontrado!)
         try {
             //PROBLEMA
-            
-             file = new File(DocComando.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().replaceAll("TMW-Maker.jar", "")+"comandos.xml");
-             //Este comando file só funciona se o arquivo comando.xml estiver na pasta do .jar
-            
-             FileReader reader = new FileReader(file);  
-               BufferedReader leitor = new BufferedReader(reader);  
-               
-            String linha = "";  
-            String linha1="";
-            while((linha1 = leitor.readLine())!=null) {  
-                linha = linha+linha1;
-            }  
-            
-            List comandos = (List) xstream.fromXML(linha);  
+            file = new File(Endereco);
+            //Este comando file só funciona se o arquivo comando.xml estiver na pasta do .jar
+
+            FileReader reader = new FileReader(file);
+            BufferedReader leitor = new BufferedReader(reader);
+
+            String linha = "";
+            String linha1 = "";
+            while ((linha1 = leitor.readLine()) != null) {
+                linha = linha + linha1;
+            }
+
+            List comandos = (List) xstream.fromXML(linha);
 
             return comandos;
         } catch (Exception ioe) {
-            JOptionPane.showMessageDialog(null,"Para funcionamento do Palco, o arquivo comandos.xml deve estar na seguinte pasta: \n"+DocComando.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().replaceAll("TMW-Maker.jar", "")+"comandos.xml","Error",0);
+            DialogClass.showErro(
+                "<HTML>O autocompletar da janela Palco só funcionará com o arquivo: \n"+Endereco,
+                "ERRO DE COMANDO AUTOCOMPLETAR"
+            );
             List<Comandos> list = new ArrayList<Comandos>();
-            
+
             list.add(new Comandos());
-            
+
             return list;
         }
         
