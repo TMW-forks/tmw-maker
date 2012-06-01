4d08dc64ff27b2dd39eaca97c1a746aad5a4a392
rui.gravata@gmail.com <rui.gravata@gmail.com@1b849053-5f8f-d5e0-c778-10af80e1e48d>
2012-06-01 13:44:48 +0000

    To tentando pagar a pasta destes arquivos mas não to conseguindo! =O

---
+++ trunk/java/gAdler32/src/bibliotecas/dfgdfgdf.java
@@ -0,0 +1,14 @@
+package bibliotecas;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lunovox
+ */
+public class dfgdfgdf {
+
+}
