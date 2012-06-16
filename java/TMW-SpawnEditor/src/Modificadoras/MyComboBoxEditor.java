8fd401caa725ba523a0571a1e33f9800d9f34583
rui.gravata <rui.gravata@gmail.com>
2012-06-16 03:08:00 +0000

    Tentativa de separar as partes do TMW-Maker. Futuramente eu editarei as partes do TMW-Maker separadamente como sendo uma lib.

---
+++ java/TMW-SpawnEditor/src/Modificadoras/MyComboBoxEditor.java
@@ -0,0 +1,11 @@
+package Modificadoras;
+
+import java.util.Vector;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
+
+public class MyComboBoxEditor extends DefaultCellEditor {
+    public MyComboBoxEditor(Vector Lista) {
+        super(new JComboBox(Lista));
+    }
+}
