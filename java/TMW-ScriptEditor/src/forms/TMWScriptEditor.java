10807108c08075c56b5e3ab161ac45dc7e4c1f5e
rui.gravata <rui.gravata@gmail.com>
2012-08-18 03:07:35 +0000

    Adaptação do projeto de versão 7.1 para Netbeans de evrsão 6.9

---
+++ java/TMW-ScriptEditor/src/forms/TMWScriptEditor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forms;
+/**
+ *
+ * @author Danilo
+ */
+public class TMWScriptEditor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new forms.TelaPrincipal().setVisible(true);
+    }
+}
