ac3689c0d1ff9c21bf30f35bb65bb88f89416591
rui.gravata@gmail.com <rui.gravata@gmail.com@1b849053-5f8f-d5e0-c778-10af80e1e48d>
2013-02-17 23:11:55 +0000

    * Estilizando Componentes

--- trunk/java/TMW-Maker2/src/formularios/FrmTMWMaker2.java
+++ trunk/java/TMW-Maker2/src/formularios/FrmTMWMaker2.java
@@ -1615,11 +1615,39 @@ public class FrmTMWMaker2 extends javax.swing.JFrame {
 	}//GEN-LAST:event_mnuEditarSequenciaActionPerformed
 
 	public static void main(String args[]) {
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("GTK+".equals(info.getName())) {
+				//if ("Nimbus".equals(info.getName())) {
+
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(FrmTMWMaker2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(FrmTMWMaker2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(FrmTMWMaker2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(FrmTMWMaker2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}/**/
+
 		java.awt.EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				FrmTMWMaker2 frmTMWMaker2 = new FrmTMWMaker2();
+				frmTMWMaker2.setExtendedState(MAXIMIZED_BOTH);
+				frmTMWMaker2.setVisible(true);
+			}
+		});
+		/*java.awt.EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				new FrmTMWMaker2().setVisible(true);
 			}
-		});
+		});/**/
 	}
    // Variables declaration - do not modify//GEN-BEGIN:variables
    private javax.swing.JButton btnAjudaForum;
